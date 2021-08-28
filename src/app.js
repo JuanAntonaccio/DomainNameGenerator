3d7b08f23b00c4a7ba417bc0658280f52d85930a
JuanAntonaccio <jmantonaccio@gmail.com>
2021-08-28 00:44:07 +0000

    subo por segunda vez

--- src/app.js
+++ src/app.js
@@ -12,11 +12,14 @@ window.onload = function() {
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
+let dominio = [".org", ".com", ".uy"];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
-      console.log(pronoun[i] + adj[j] + noun[k]);
+      for (let l = 0; l < dominio.length; l++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + dominio[l]);
+      }
     }
   }
 }
