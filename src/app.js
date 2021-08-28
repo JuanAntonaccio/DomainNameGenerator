83259ea5d9bbf142d78ccfb98e754c4a1fa62d1b
JuanAntonaccio <jmantonaccio@gmail.com>
2021-08-28 01:06:23 +0000

    subo con listas

--- src/app.js
+++ src/app.js
@@ -13,13 +13,16 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let dominio = [".org", ".com", ".uy"];
-
+let domi = document.getElementById("domi");
+let lista = "<ol>";
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       for (let l = 0; l < dominio.length; l++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + dominio[l]);
+        lista += "<li>" + pronoun[i] + adj[j] + noun[k] + dominio[l] + "</li>";
       }
     }
   }
 }
+lista += "</ol>";
+domi.innerHTML = lista;
